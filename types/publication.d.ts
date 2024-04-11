2580c7be47cffe1380c3a839dda2fae4f2d97ee8
Augusto Vicente <devguto@gmail.com>
2024-04-11 17:18:26 +0100

    FIX: publication - get by slug missing return properties

--- types/publication.d.ts
+++ types/publication.d.ts
@@ -5,6 +5,33 @@ import { CreateNewDraftResponse } from "./draft";
  */
 type GetPublicationBySlugResult = CreateNewDraftResponse & {
     state: 'DRAFT' | 'PUBLISHED' | 'SCHEDULED' | 'UNPUBLISHED' | 'QUARANTINED',
+    /**
+     * The identifier of the file that will be used in the next publish
+     */
+    file?: number,
+    /**
+     * PUBLIC documents, once published, will be made searcheable and will appear in streams, recommendations, etc. PRIVATE documents, once published, are only accessible from users that knows their URL.
+     */
+    access?: DraftAccess,
+    title?: string,
+    description?: string,
+    /**
+     * Set it to true to indicate that the document is a preview of a bigger content.
+     */
+    preview?: boolean,
+    type?: DraftType,
+    /**
+     * When set to true, the conversion procedure will search for hypermedia links inside the document text.
+     */
+    showDetectedLinks?: boolean,
+    /**
+     * When set to true once published the readers will be allowed to download the original document.
+     */
+    downloadable?: boolean,
+    /**
+     * Set the original publish date field to indicate that a document was previously published, e.g. to import older issues of your magazine. Set it to null to clear the backDate.
+     */
+    originalPublishDate?: string,
 };
 
 type Link = {
