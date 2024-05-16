24da83d40e60130bdbcff38762d5fd4e7c0ce736
Augusto Vicente <devguto@gmail.com>
2024-05-16 10:25:14 +0100

    FIX: add missing publiclocation in the create new draft response

--- types/document.d.ts
+++ types/document.d.ts
@@ -69,6 +69,10 @@ interface Document {
      */
     location: string;
     /**
+     * The document page URL
+     */
+    publicLocation?: string;
+    /**
      * Changes to be apply on the next publish
      */
     changes?: DocumentInformation;
